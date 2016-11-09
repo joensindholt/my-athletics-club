3ca35ea94de5860dd17a9fef1b4dba0a9cb445de
Joen Sindholt <joensindholt@gmail.com>
2016-11-09 21:59:40 +0100

    adding deploy script

---
+++ update-live.sh
@@ -0,0 +1,39 @@
+# settings
+basepath=~/dev/my-athletics-club
+
+# go to base path
+cd $basepath
+
+# get latest
+git pull
+
+# stop webserver
+sudo apache2ctl stop
+
+# update frontend
+cd $basepath/frontend
+npm update
+bower update
+tsd reinstall -so
+gulp --settings='production'
+sudo rm -R /var/www/my-athletics-club/*
+sudo cp -R dist/* /var/www/my-athletics-club
+
+# update backend
+sudo forever stopall
+cd $basepath/api
+tsc
+sudo forever start 'dist/server.js'
+
+# start webserver
+sudo apache2ctl start
+
+# list stuff
+echo --------------------------------
+echo All done. 
+echo Below is the forever list showing whether the api is up and running or not. 
+echo Please check https://gik.majig.dk too.
+echo Have a nice day :)
+echo --------------------------------
+sudo forever list
+echo --------------------------------

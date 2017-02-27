c0813d288271f75f4d98f7aecb7feae190181442
Joen Sindholt <joensindholt@gmail.com>
2017-02-27 20:04:39 +0100

    deleting old files

--- frontend2/src/client/assets/svg/readme.md
+++
@@ -1,3 +0,0 @@
-# Icons
-
-Downloaded from: http://www.flaticon.com/packs/for-your-interface-2
--- frontend2/tools/env/dev.ts
+++
@@ -1,9 +0,0 @@
-import { EnvConfig } from './env-config.interface';
-
-const DevConfig: EnvConfig = {
-  ENV: 'DEV',
-  API: 'http://localhost:8889'
-};
-
-export = DevConfig;
-

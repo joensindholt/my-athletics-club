12f40c9cf4ea245aab6bb9a6d23131f3d58682c1
Joen Sindholt <joensindholt@gmail.com>
2017-05-04 21:22:17 +0200

    Fixing unit tests

--- api/test/MyAthleticsClub.Api.UnitTests/Endpoints/Users/Services/UserControllerTests.cs
+++ api/test/MyAthleticsClub.Api.UnitTests/Endpoints/Users/Services/UserControllerTests.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using MyAthleticsClub.Api.Controllers;
 using MyAthleticsClub.Api.Core.Authentication;
 using MyAthleticsClub.Core.Models;
 using MyAthleticsClub.Core.Services.Interfaces;
 using Newtonsoft.Json.Linq;
 using NSubstitute;
+using System;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -24,11 +27,11 @@ namespace MyAthleticsClub.Api.UnitTests.Endpoints.Users.Services
             var userServiceMock = Substitute.For<IUserService>();
             userServiceMock.TryGetClaimsIdentityAsync(user).Returns(new ClaimsIdentity());
 
-            var optionsMock = Substitute.For<IOptions<JwtIssuerOptions>>();
-
-            var loggerMock = Substitute.For<ILogger<UserController>>();
-
-            var userController = new UserController(optionsMock, loggerMock, userServiceMock);
+            var userController = 
+                new UserController(
+                    Options.Create(GetTestJwtOptions()),
+                    Substitute.For<ILogger<UserController>>(), 
+                    userServiceMock);
 
             // Act
             var result = await userController.Login(user);
@@ -37,6 +40,21 @@ namespace MyAthleticsClub.Api.UnitTests.Endpoints.Users.Services
             Assert.True(result is OkObjectResult, "An OK response was expected");
             Assert.True(((OkObjectResult)result).Value is string);
             Assert.True(JObject.Parse(((string)((OkObjectResult)result).Value))["access_token"].Value<string>().Length > 0);
+        }
+
+        private static JwtIssuerOptions GetTestJwtOptions()
+        {
+            const string sec = "401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1";
+            var now = DateTime.UtcNow;
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(sec));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var jwtOptions = new JwtIssuerOptions
+            {
+                ValidFor = TimeSpan.FromHours(1),
+                SigningCredentials = signingCredentials
+            };
+            return jwtOptions;
         }
     }
 }

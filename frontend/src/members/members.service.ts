491c28392ba4db17e88e4b2a5286055ca50f2ef2
Joen Sindholt <joensindholt@gmail.com>
2020-01-29 21:57:23 +0100

    Make frontend show CFR statistics (#74)
    * Make solution work
    
    * Changed statistics page to do CFR statistics

--- frontend/src/members/members.service.ts
+++ frontend/src/members/members.service.ts
@@ -4,28 +4,26 @@ module members {
   'use strict';
 
   export class MembersService {
-
-    private API_PATH = globals.apiUrl;;
+    private API_PATH = globals.apiUrl;
 
     static $inject = ['$http', '$q'];
 
-    constructor(
-      private $http: ng.IHttpService,
-      private $q: ng.IQService
-    ) {
-    }
+    constructor(private $http: ng.IHttpService, private $q: ng.IQService) {}
 
     getAll(): ng.IPromise<Array<Member>> {
       var deferred = this.$q.defer<Array<Member>>();
 
-      this.$http.get(this.API_PATH + '/members').then((response: any) => {
-        var members = _.map(response.data.items, (memberData) => {
-          return new Member(memberData);
+      this.$http
+        .get(this.API_PATH + '/members')
+        .then((response: any) => {
+          var members = _.map(response.data.items, memberData => {
+            return new Member(memberData);
+          });
+          deferred.resolve(members);
+        })
+        .catch(err => {
+          deferred.reject(err);
         });
-        deferred.resolve(members)
-      }).catch(err => {
-        deferred.reject(err);
-      });
 
       return deferred.promise;
     }
@@ -33,14 +31,17 @@ module members {
     getTerminatedMembers(): ng.IPromise<Array<Member>> {
       var deferred = this.$q.defer<Array<Member>>();
 
-      this.$http.get(this.API_PATH + '/members/terminated').then((response: any) => {
-        var members = _.map(response.data.items, (memberData) => {
-          return new Member(memberData);
+      this.$http
+        .get(this.API_PATH + '/members/terminated')
+        .then((response: any) => {
+          var members = _.map(response.data.items, memberData => {
+            return new Member(memberData);
+          });
+          deferred.resolve(members);
+        })
+        .catch(err => {
+          deferred.reject(err);
         });
-        deferred.resolve(members)
-      }).catch(err => {
-        deferred.reject(err);
-      });
 
       return deferred.promise;
     }
@@ -51,11 +52,13 @@ module members {
       member.id = '-1';
 
       // ... and post to server
-      this.$http.post(this.API_PATH + '/members', member)
+      this.$http
+        .post(this.API_PATH + '/members', member)
         .then(response => {
           var newMember = new Member(response.data);
-          deferred.resolve(newMember)
-        }).catch(err => {
+          deferred.resolve(newMember);
+        })
+        .catch(err => {
           deferred.reject(err);
         });
 
@@ -65,12 +68,15 @@ module members {
     get(id: string): ng.IPromise<Member> {
       var deferred = this.$q.defer<Member>();
 
-      this.$http.get(this.API_PATH + '/members/' + id).then((response: any) => {
-        var member = new Member(response.data);
-        deferred.resolve(member);
-      }).catch(err => {
-        deferred.reject(err);
-      })
+      this.$http
+        .get(this.API_PATH + '/members/' + id)
+        .then((response: any) => {
+          var member = new Member(response.data);
+          deferred.resolve(member);
+        })
+        .catch(err => {
+          deferred.reject(err);
+        });
 
       return deferred.promise;
     }
@@ -78,11 +84,14 @@ module members {
     update(member: Member): ng.IPromise<Member> {
       var deferred = this.$q.defer<Member>();
 
-      this.$http.put(this.API_PATH + '/members/' + member.id, member).then(response => {
-        deferred.resolve();
-      }).catch(err => {
-        deferred.reject(err);
-      })
+      this.$http
+        .put(this.API_PATH + '/members/' + member.id, member)
+        .then(response => {
+          deferred.resolve();
+        })
+        .catch(err => {
+          deferred.reject(err);
+        });
 
       return deferred.promise;
     }
@@ -96,7 +105,7 @@ module members {
       for (var i = maximumYear; i >= minimumYear; i--) {
         birthYears.push(i);
       }
-      birthYears.push((minimumYear - 1) + ' eller før');
+      birthYears.push(minimumYear - 1 + ' eller før');
 
       return birthYears;
     }
@@ -104,11 +113,14 @@ module members {
     chargeMemberships(): ng.IPromise<{}> {
       var deferred = this.$q.defer<Member>();
 
-      this.$http.post(this.API_PATH + '/members/charge-all', {}).then(response => {
-        deferred.resolve();
-      }).catch(err => {
-        deferred.reject(err);
-      })
+      this.$http
+        .post(this.API_PATH + '/members/charge-all', {})
+        .then(response => {
+          deferred.resolve();
+        })
+        .catch(err => {
+          deferred.reject(err);
+        });
 
       return deferred.promise;
     }
@@ -116,14 +128,17 @@ module members {
     terminateMembership(id: string, terminationDate: string): ng.IPromise<{}> {
       var deferred = this.$q.defer<Member>();
 
-      this.$http.post(this.API_PATH + '/members/terminate', {
-        memberId: id,
-        terminationDate: terminationDate
-      }).then(response => {
-        deferred.resolve();
-      }).catch(err => {
-        deferred.reject(err);
-      })
+      this.$http
+        .post(this.API_PATH + '/members/terminate', {
+          memberId: id,
+          terminationDate: terminationDate
+        })
+        .then(response => {
+          deferred.resolve();
+        })
+        .catch(err => {
+          deferred.reject(err);
+        });
 
       return deferred.promise;
     }
@@ -163,11 +178,29 @@ module members {
     getStatistics(date: string): ng.IPromise<any> {
       var deferred = this.$q.defer<Member>();
 
-      this.$http.get(this.API_PATH + '/members/statistics?date=' + date).then((response: any) => {
-        deferred.resolve(response.data);
-      }).catch(err => {
-        deferred.reject(err)
-      });
+      this.$http
+        .get(this.API_PATH + '/members/statistics?date=' + date)
+        .then((response: any) => {
+          deferred.resolve(response.data);
+        })
+        .catch(err => {
+          deferred.reject(err);
+        });
+
+      return deferred.promise;
+    }
+
+    getCfrStatistics(year: string): ng.IPromise<any> {
+      var deferred = this.$q.defer<Member>();
+
+      this.$http
+        .get(this.API_PATH + '/members/statistics/cfr.json?year=' + year)
+        .then((response: any) => {
+          deferred.resolve(response.data);
+        })
+        .catch(err => {
+          deferred.reject(err);
+        });
 
       return deferred.promise;
     }
@@ -175,16 +208,18 @@ module members {
     getAvailableFamilyMembershipNumber(): ng.IPromise<Number> {
       var deferred = this.$q.defer<Number>();
 
-      this.$http.get(this.API_PATH + '/members/available-family-membership-number').then((response: any) => {
-        deferred.resolve(response.data.number);
-      }).catch(err => {
-        deferred.reject(err)
-      });
-      
+      this.$http
+        .get(this.API_PATH + '/members/available-family-membership-number')
+        .then((response: any) => {
+          deferred.resolve(response.data.number);
+        })
+        .catch(err => {
+          deferred.reject(err);
+        });
+
       return deferred.promise;
     }
   }
 }
 
-angular.module('members')
-  .service('MembersService', members.MembersService);
+angular.module('members').service('MembersService', members.MembersService);

69593989dbc10a391e03104ea98aa6a8ae182822
Joen Sindholt <joensindholt@gmail.com>
2020-01-29 21:04:45 +0100

    Make solution work

--- frontend/gulp_tasks/gulp.config.js
+++ frontend/gulp_tasks/gulp.config.js
@@ -15,54 +15,46 @@ module.exports = {
       globalPath.app + "**/*.svg",
       globalPath.app + "**/files/*.*"
     ],
-    sass: [
-      globalPath.app + '**/*.scss'
-    ],
-    img: [
-      globalPath.app + "img/**/*",
-    ],
-    tests: [
-      globalPath.app + "**/*_test.js"
-    ]
+    sass: [globalPath.app + "**/*.scss"],
+    img: [globalPath.app + "img/**/*"],
+    tests: [globalPath.app + "**/*_test.js"]
   },
   libs: {
     script: [
-      globalPath.bower + 'jquery/dist/jquery.min.js',
-      globalPath.bower + 'angular/angular.js',
-      globalPath.bower + 'angular-route/angular-route.js',
-      globalPath.bower + 'angular-sanitize/angular-sanitize.min.js',
-      globalPath.bower + 'angular-ui-router/release/angular-ui-router.js',
-      globalPath.bower + 'angular-aria/angular-aria.js',
-      globalPath.bower + 'angular-animate/angular-animate.js',
-      globalPath.bower + 'raven-js/dist/raven.js',
-      globalPath.bower + 'raven-js/dist/plugins/angular.js',
-      globalPath.bower + 'lodash/dist/lodash.min.js',
-      globalPath.bower + 'moment/min/moment-with-locales.min.js',
-      globalPath.bower + 'checklist-model/checklist-model.js',
-      globalPath.bower + 'angular-recaptcha/release/angular-recaptcha.js',
-      globalPath.bower + 'angular-hotkeys/build/hotkeys.min.js',
+      globalPath.bower + "jquery/dist/jquery.min.js",
+      globalPath.bower + "angular/angular.js",
+      globalPath.bower + "angular-route/angular-route.js",
+      globalPath.bower + "angular-sanitize/angular-sanitize.min.js",
+      globalPath.bower + "angular-ui-router/release/angular-ui-router.js",
+      globalPath.bower + "angular-aria/angular-aria.js",
+      globalPath.bower + "angular-animate/angular-animate.js",
+      globalPath.bower + "raven-js/dist/raven.js",
+      globalPath.bower + "raven-js/dist/plugins/angular.js",
+      globalPath.bower + "lodash/lodash.js",
+      globalPath.bower + "moment/min/moment-with-locales.min.js",
+      globalPath.bower + "checklist-model/checklist-model.js",
+      globalPath.bower + "angular-recaptcha/release/angular-recaptcha.js",
+      globalPath.bower + "angular-hotkeys/build/hotkeys.min.js",
       // Auth0 scripts
-      globalPath.bower + 'auth0-lock/build/lock.js',
-      globalPath.bower + 'angular-lock/dist/angular-lock.js',
-      globalPath.bower + 'angular-jwt/dist/angular-jwt.js',
+      globalPath.bower + "auth0-lock/build/lock.js",
+      globalPath.bower + "angular-lock/dist/angular-lock.js",
+      globalPath.bower + "angular-jwt/dist/angular-jwt.js",
       // ---
-      globalPath.bower + 'angular-autogrow/angular-autogrow.min.js',
-      globalPath.bower + 'angular-bootstrap/ui-bootstrap-tpls.min.js',
-      globalPath.bower + 'toastr/toastr.min.js',
-      globalPath.bower + 'ng-csv/build/ng-csv.min.js',
-      globalPath.npm + 'chart.js/dist/Chart.min.js',
-      globalPath.npm + 'angular-chart.js/dist/angular-chart.min.js'
+      globalPath.bower + "angular-autogrow/angular-autogrow.min.js",
+      globalPath.bower + "angular-bootstrap/ui-bootstrap-tpls.min.js",
+      globalPath.bower + "toastr/toastr.min.js",
+      globalPath.bower + "ng-csv/build/ng-csv.min.js",
+      globalPath.npm + "chart.js/dist/Chart.min.js",
+      globalPath.npm + "angular-chart.js/dist/angular-chart.min.js"
     ],
     css: [
-      globalPath.bower + 'bootstrap/dist/css/bootstrap.min.css',
-      globalPath.bower + 'font-awesome/css/font-awesome.min.css',
-      globalPath.bower + 'angular-bootstrap/ui-bootstrap-csp.css',
-      globalPath.bower + 'angular-hotkeys/build/hotkeys.min.css',
-      globalPath.bower + 'toastr/toastr.min.css'
+      globalPath.bower + "bootstrap/dist/css/bootstrap.min.css",
+      globalPath.bower + "font-awesome/css/font-awesome.min.css",
+      globalPath.bower + "angular-bootstrap/ui-bootstrap-csp.css",
+      globalPath.bower + "angular-hotkeys/build/hotkeys.min.css",
+      globalPath.bower + "toastr/toastr.min.css"
     ],
-    fonts: [
-      globalPath.bower + 'font-awesome/fonts/*.*'
-    ],
+    fonts: [globalPath.bower + "font-awesome/fonts/*.*"],
     sass: []
   },
   dist: "./dist/"

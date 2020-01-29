b61758029e93cdee8d0a6be024e61641fe851f97
Joen Sindholt <joensindholt@gmail.com>
2020-01-29 21:55:35 +0100

    Changed statistics page to do CFR statistics

--- frontend/src/members/member.statistics.controller.ts
+++ frontend/src/members/member.statistics.controller.ts
@@ -6,8 +6,7 @@ module members {
   'use strict';
 
   export class MemberStatisticsController {
-
-    date: string;
+    year: string;
     statistics: any;
     totals: any;
 
@@ -15,40 +14,33 @@ module members {
     series: any;
     data: any;
 
-    static $inject = [
-      'moment',
-      'MembersService'
-    ];
+    static $inject = ['moment', 'MembersService'];
 
-    constructor(
-      private moment: moment.MomentStatic,
-      private MembersService: MembersService
-    ) {
-      this.date = this.moment().format('YYYY-MM-DD');
+    constructor(private moment: moment.MomentStatic, private MembersService: MembersService) {
+      this.year = this.moment()
+        .subtract('years', 1)
+        .format('YYYY');
       this.update();
-
     }
 
     update() {
-      this.MembersService.getStatistics(this.date).then(statistics => {
+      this.MembersService.getCfrStatistics(this.year).then(statistics => {
+        console.log(statistics);
+
         this.statistics = statistics;
 
         this.totals = {
-          females: _.sumBy(statistics, (i: any) => i.genders.females),
-          males: _.sumBy(statistics, (i: any) => i.genders.males),
-          total: _.sumBy(statistics, (i: any) => i.genders.females + i.genders.males)
-        }
+          females: _.sumBy(statistics, (i: any) => i.females),
+          males: _.sumBy(statistics, (i: any) => i.males),
+          total: _.sumBy(statistics, (i: any) => i.females + i.males)
+        };
 
-        this.labels = _.map(this.statistics, (s: any) => s.age + ' år');;
+        this.labels = _.map(this.statistics, (s: any) => s.age + ' år');
         this.series = ['Piger', 'Drenge'];
-        this.data = [
-          _.map(this.statistics, (s: any) => s.genders.females),
-          _.map(this.statistics, (s: any) => s.genders.males)
-        ];
+        this.data = [_.map(this.statistics, (s: any) => s.females), _.map(this.statistics, (s: any) => s.males)];
       });
     }
   }
 }
 
-angular.module('members')
-  .controller('MemberStatisticsController', members.MemberStatisticsController);
+angular.module('members').controller('MemberStatisticsController', members.MemberStatisticsController);

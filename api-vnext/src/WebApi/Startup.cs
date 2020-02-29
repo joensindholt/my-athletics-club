761bb9928fba29c391637c42d8135b0e023c5276
Joen Sindholt <joensindholt@gmail.com>
2020-02-29 19:29:51 +0100

    Remove unused using statements

--- api-vnext/src/WebApi/Startup.cs
+++ api-vnext/src/WebApi/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using MyAthleticsClub.Api.Application;
 using MyAthleticsClub.Api.Infrastructure;
 using MyAthleticsClub.Api.WebApi.Features.OpenApi;
@@ -25,7 +18,6 @@ namespace WebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplication();
@@ -34,7 +26,6 @@ namespace WebApi
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -43,11 +34,8 @@ namespace WebApi
             }
 
             app.UseHttpsRedirection();
-
             app.UseOpenApiWithUI();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

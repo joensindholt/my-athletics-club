348c5adc207a3cf3a2260fc3871b8c3cf98dd905
Joen Sindholt <joensindholt@gmail.com>
2020-03-06 19:38:06 +0100

    Delete old attempt

--- frontend2/src/app/core/user.ts
+++
@@ -1,5 +0,0 @@
-export interface User {
-  username: string,
-  expires: number,
-  access_token: string
-}
--- frontend2/src/app/subscription/subscription.ts
+++
@@ -1,9 +0,0 @@
-export class Subscription {
-  id: string;
-  title: string;
-  price: number;
-  balance: number;
-  reminder?: number;
-  lastReminder?: number;
-  latestInvoiceDate: string;
-}
